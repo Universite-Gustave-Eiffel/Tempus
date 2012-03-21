0f83bbfc59745bb4818768a675f1a967686f1196
Vincent Picavet <vincent.picavet@oslandia.com>
2012-03-21 14:38:56 +0100

    Add unit testing for multinet loader

---
+++ src/loader/tests/test_multinet.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Tempus data loader
+# (c) 2012 Oslandia
+# MIT Licence
+
+import unittest
+import tempus
+import os
+
+class TestMultinetLoader(unittest.TestCase):
+
+    def setUp(self):
+        self.multinet = "%s/../../../data/multinet" % os.path.dirname(os.path.realpath(__file__))
+        self.ml = tempus.MultinetLoader(self.multinet, "usaxxxxx______", "testmnet")
+
+    def test_check_input(self):
+        # test input checker 
+        res, files = self.ml.check_input()
+        self.assertEqual(res, True)
+        self.assertEqual(files, [])
+
+    def test_get_shapefiles(self):
+        res = ['/home/vpicavet/oslandia/local/projets/tempus/data/multinet/usaxxxxx______nw.shp',
+ '/home/vpicavet/oslandia/local/projets/tempus/data/multinet/usaxxxxx______jc.shp',
+ '/home/vpicavet/oslandia/local/projets/tempus/data/multinet/usaxxxxx______mn.shp',
+ '/home/vpicavet/oslandia/local/projets/tempus/data/multinet/usaxxxxx______cf.shp']
+        self.ml.get_shapefiles()
+        self.assertEqual(self.ml.shapefiles, res)
+
+    def test_load(self):
+        ret = self.ml.load()
+        self.assertEqual(ret, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
