bbe7f738571fe4eb4225c963fe86dc1ca103886c
Hugo Mercier <hugo.mercier@oslandia.com>
2012-03-23 09:45:39 +0100

    Made road_graph an undirected graph, instead of a directed one. Introduces a segfault on program termination ...

--- src/core/road_graph.hh
+++ src/core/road_graph.hh
@@ -31,13 +31,13 @@ namespace Tempus
 	/// depending on VertexListType and EdgeListType used to represent lists of vertices (vecS, listS, etc.)
 	typedef boost::mpl::if_<typename boost::detail::is_random_access<VertexListType>::type, size_t, void*>::type Vertex;
 	/// see adjacency_list.hpp
-	typedef boost::detail::edge_desc_impl<boost::directed_tag, Vertex> Edge;
+	typedef boost::detail::edge_desc_impl<boost::undirected_tag, Vertex> Edge;
 
 	struct Node;
 	struct Section;
 	///
 	/// The final road graph type
-	typedef boost::adjacency_list<VertexListType, EdgeListType, boost::directedS, Node, Section > Graph;
+	typedef boost::adjacency_list<VertexListType, EdgeListType, boost::undirectedS, Node, Section > Graph;
 
 	///
 	/// Used as Vertex.
