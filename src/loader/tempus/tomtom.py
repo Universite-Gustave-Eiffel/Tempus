c854f3106de4ee0353f492c665ecb0394758f49b
Vincent Picavet <vincent.picavet@oslandia.com>
2012-03-27 15:10:58 +0200

    Remove duplicate cf table and add postload SQL.

--- src/loader/tempus/tomtom.py
+++ src/loader/tempus/tomtom.py
@@ -13,9 +13,9 @@ from importer import ShpImporter
 class MultinetImporter(ShpImporter):
     """This class enables to load TomTom Multinet data into a PostGIS database."""
     # Shapefile names to load, without the extension and prefix. It will be the table name.
-    SHAPEFILES = ['nw', 'jc', 'mn', 'cf', '2r', 'rn', 'mp', 'is', 'ig', 'cf', 'rs', 'td', 'sr'] 
+    SHAPEFILES = ['nw', 'jc', 'mn', 'cf', '2r', 'rn', 'mp', 'is', 'ig', 'rs', 'td', 'sr']
     # SQL files to execute before loading shapefiles
     PRELOADSQL = ["reset_import_schema.sql"]
     # SQL files to execute after loading shapefiles 
-    POSTLOADSQL = []
+    POSTLOADSQL = ["multinet.sql"]
 
