cc2d9bc556beec51edbe5da27dace5db544fc6c9
Vincent Picavet <vincent.picavet@oslandia.com>
2012-04-12 16:35:11 +0200

    Add first qgis plugin draft.

---
+++ src/wps_qgis/IfsttarRouting/ifsttarrouting.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ IfsttarRouting
+                                 A QGIS plugin
+ Get routing informations with various algorithms
+                              -------------------
+        begin                : 2012-04-12
+        copyright            : (C) 2012 by Oslandia
+        email                : hugo.mercier@oslandia.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+# Import the PyQt and QGIS libraries
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from qgis.core import *
+# Initialize Qt resources from file resources.py
+import resources_rc
+# Import the code for the dialog
+from ifsttarroutingdialog import IfsttarRoutingDialog
+
+class IfsttarRouting:
+
+    def __init__(self, iface):
+        # Save reference to the QGIS interface
+        self.iface = iface
+        # Create the dialog and keep reference
+        self.dlg = IfsttarRoutingDialog()
+        # initialize plugin directory
+        self.plugin_dir = QFileInfo(QgsApplication.qgisUserDbFilePath()).path() + "/python/plugins/ifsttarrouting"
+        # initialize locale
+        localePath = ""
+        locale = QSettings().value("locale/userLocale").toString()[0:2]
+       
+        if QFileInfo(self.plugin_dir).exists():
+            localePath = self.plugin_dir + "/i18n/ifsttarrouting_" + locale + ".qm"
+
+        if QFileInfo(localePath).exists():
+            self.translator = QTranslator()
+            self.translator.load(localePath)
+
+            if qVersion() > '4.3.3':
+                QCoreApplication.installTranslator(self.translator)
+   
+
+    def initGui(self):
+        # Create action that will start plugin configuration
+        self.action = QAction(QIcon(":/plugins/ifsttarrouting/icon.png"), \
+            u"Compute route", self.iface.mainWindow())
+        # connect the action to the run method
+        QObject.connect(self.action, SIGNAL("triggered()"), self.run)
+
+        # Add toolbar button and menu item
+        self.iface.addToolBarIcon(self.action)
+        self.iface.addPluginToMenu(u"&Compute route", self.action)
+
+    def unload(self):
+        # Remove the plugin menu item and icon
+        self.iface.removePluginMenu(u"&Compute route",self.action)
+        self.iface.removeToolBarIcon(self.action)
+
+    # run method that performs all the real work
+    def run(self):
+        # show the dialog
+        self.dlg.show()
+        # Run the dialog event loop
+        result = self.dlg.exec_()
+        # See if OK was pressed
+        if result == 1:
+            # do something useful (delete the line containing pass and
+            # substitute with your code)
+            pass
