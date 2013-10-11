db883170a9c7731bb1c1a2913b1a270b0311f179
vmora <vincent.mora@oslandia.com>
2013-10-11 16:52:29 +0200

    moved package inclusion in top level CMakeLists fixed test for windows

--- src/tests/main.cc
+++ src/tests/main.cc
@@ -1,6 +1,8 @@
 #define BOOST_TEST_MODULE UnitTestTempus
 #define BOOST_TEST_ALTERNATIVE_INIT_API
-#define BOOST_TEST_DYN_LINK
+#ifndef _WIN32
+#   define BOOST_TEST_DYN_LINK
+#endif
 #include <boost/test/unit_test.hpp>
 
 using namespace boost::unit_test ;
