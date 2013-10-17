3f9dc94e4cfff2d7e45687ff5a6335ad7cf92e7f
Hugo Mercier <hugo.mercier@oslandia.com>
2013-10-17 16:36:44 +0200

    Move Python WKB class to a standalone file

--- src/hmi_qgis/IfsttarRouting/ifsttarrouting.py
+++ src/hmi_qgis/IfsttarRouting/ifsttarrouting.py
@@ -26,7 +26,6 @@ import config
 import binascii
 import pickle
 import os
-import struct
 import math
 from datetime import datetime
 
@@ -51,6 +50,7 @@ from ui_splash_screen import Ui_SplashScreen
 from history_file import ZipHistoryFile
 from result_selection import ResultSelection
 from altitude_profile import AltitudeProfile
+from wkb import WKB
 
 import pickle
 
@@ -60,44 +60,6 @@ ROADMAP_LAYER_NAME = "Roadmap_"
 
 # There has been an API change regarding vector layer on 1.9 branch
 NEW_API = 'commitChanges' in dir(QgsVectorLayer)
-
-class WKB:
-
-    def __init__( self, wkb ):
-        self.wkb = wkb
-        # prefix for unpack : little or big endian
-        self.prefix = '<' if wkb[1] == '1' else '>'
-        self.type = struct.unpack(self.prefix+'h', self.wkb[1*2:3*2].decode('hex'))[0]
-
-    # convert a LineStringZ to a 2D LineString
-    def force2d( self ):
-        if self.type != 1002: # LineStringZ
-            return
-        nwkb = self.wkb[0:2] + '0200' + self.wkb[3*2:9*2]
-        npts = struct.unpack(self.prefix+'h', self.wkb[5*2:7*2].decode('hex'))[0]
-        for i in range(0,npts):
-            p = 9+i*3*8
-            xs = self.wkb[p*2:(p+8)*2]
-            ys = self.wkb[(p+8)*2:(p+16)*2]
-            nwkb += xs + ys
-        return nwkb
-
-    # get an array of (x,y,z) from a LineStringZ
-    def dumpPoints( self ):
-        if self.type != 1002:
-            return []
-        ret = []
-        npts = struct.unpack(self.prefix+'h', self.wkb[5*2:7*2].decode('hex'))[0]
-        for i in range(0,npts):
-            p = 9+i*3*8
-            xs = self.wkb[p*2:(p+8)*2]
-            ys = self.wkb[(p+8)*2:(p+16)*2]
-            zs = self.wkb[(p+16)*2:(p+24)*2]
-            x = struct.unpack(self.prefix+'d', xs.decode('hex'))[0]
-            y = struct.unpack(self.prefix+'d', ys.decode('hex'))[0]
-            z = struct.unpack(self.prefix+'d', zs.decode('hex'))[0]
-            ret.append( (x,y,z) )
-        return ret
 
 def format_cost( cost ):
     cost_value = float(cost.attrib['value'])
