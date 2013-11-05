5315f57b20d2e1648b50b4a0a56065f547ba475e
vmora <vincent.mora@oslandia.com>
2013-11-05 09:23:43 +0100

    debugged find for windows

--- cmake/Modules/FindFCGI.cmake
+++ cmake/Modules/FindFCGI.cmake
@@ -13,31 +13,26 @@
 
 find_path(FCGI_INCLUDE_DIR NAME fcgio.h
    PATHS
-   $ENV{FCGI_ROOT}
+   $ENV{FCGI_ROOT}/include
    )
 
 find_library(FCGI_LIBRARIES NAMES fcgi fcgi++ libfcgi 
    PATHS
-   $ENV{FCGI_ROOT}
+   $ENV{FCGI_ROOT}/libfcgi/Release
    )
 
 if (UNIX)
-    find_library(FCGI_CPP_LIBRARIES NAMES fcgi++ 
-        PATHS
-        $ENV{FCGI_ROOT}
-        )
-else()
-    set(FCGI_CPP_LIBRARIES "")
+    find_library(FCGI_CPP_LIBRARIES NAMES fcgi++ )
 endif()
 
-if (FCGI_INCLUDE_DIR AND FCGI_LIBRARIES AND FCGI_CPP_LIBRARIES)
+if (FCGI_INCLUDE_DIR AND FCGI_LIBRARIES AND ((NOT UNIX) OR FCGI_CPP_LIBRARIES))
     set(FCGI_LIBRARIES ${FCGI_LIBRARIES} ${FCGI_CPP_LIBRARIES})
     set(FCGI_FOUND 1)
     message( STATUS "Found fcgi: " ${FCGI_LIBRARIES} )
 else()
-    if (_FIND_REQUIRED)
+    if (FCGI_FIND_REQUIRED)
         message( FATAL_ERROR "fcgi not found" )
     else()
-        message( ERROR "fcgi not found" )
+        message( WARNING "fcgi not found" )
     endif()
 endif()
