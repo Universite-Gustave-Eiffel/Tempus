a8ff3c2ecae8bee6ea3b1e39ed50e563936338fb
vmora <vincent.mora@oslandia.com>
2013-11-08 09:50:54 +0100

    install script for ubuntu

--- script/install-tempus.sh
+++ script/install-tempus.sh
@@ -1,51 +1,74 @@
-sudo apt-get install nginx postgresql libpq-dev postgis g++ libboost-all-dev libglib2.0-dev libfcgi-dev libshp-dev libxml2-dev cmake cmake-curses-gui pyqt4-dev-tools
+sudo apt-get install nginx postgresql libpq-dev g++ libboost-all-dev libglib2.0-dev libfcgi-dev libshp-dev libxml2-dev cmake cmake-curses-gui pyqt4-dev-tools libtool
 
-# begin database configuration as user posgres
-    sudo su - postgres
-	dropdb tempus_test_db
-	createdb tempus_test_db
-	psql tempus_test_db -c "CREATE EXTENSION postgis" # TODO: fix for postgres 9.1
-	psql tempus_test_db < /usr/local/share/tempus/data/tempus_test_db.sql
+TEMPUS_DATA_DIRECTORY=/usr/local/share/tempus/data
 
-	# wps is run as deamon under user demon, so we need to create a role for this user in the test database
-	psql tempus_test_db -c "CREATE USER daemon"
-	psql tempus_test_db -c "GRANT USAGE ON SCHEMA public,tempus TO daemon"
-        psql tempus_test_db -c "GRANT SELECT ON ALL TABLES IN SCHEMA public,tempus TO daemon"
-    logout 
-# end database configuration
+die()
+{
+    echo $1 1>&2
+    exit 1
+}
 
-# begin compile and install tempus
-    #wget http://tempus.ifsttar.fr/tempus-0.9.0.tar.gz # TODO: uncomment and give proper adress
-    tar -xvzf tempus-0.9.0.tar.gz
-    cd tempus-0.9.0
-    mkdir build
-    cd build
-    cmake .. && make && sudo make install
-    ldconfig
+echo "downloading, compiling and installing tempus"
+    #wget http://tempus.ifsttar.fr/tempus-0.9.0.tar.gz && # TODO: uncomment and give proper adress
+    tar -xvzf tempus-0.9.0.tgz &&
+    mkdir tempus-0.9.0/build &&
+    cd tempus-0.9.0/build &&
+    cmake .. && make  && sudo make install || die "cannot install tempus"
+    cd ../..
+    sudo ldconfig 
+echo "tempus has been successfully installed"
 
+echo "downloading, compiling and installing postgis"
+   wget http://download.osgeo.org/postgis/source/postgis-2.1.0.tar.gz &&
+   tar -xvzf postgis-2.1.0.tar.gz &&
+   cd postgis-2.1.0 &&
+   ./configure && make && sudo make install || die "cannot install postgis"
+   cd ..
+echo "postgis has been successfully installed"
+
+echo "configuring test database"
+    # create the database and populate it
+    sudo su - postgres -c "
+        dropdb tempus_test_db;
+        createdb tempus_test_db;
+        psql tempus_test_db -c 'CREATE EXTENSION postgis';
+        psql tempus_test_db < $TEMPUS_DATA_DIRECTORY/tempus_test_db.sql
+    "
+    # wps is run as deamon, so we need to create a role for user 'daemon' in the test database
+    sudo su - postgres -c "
+        psql tempus_test_db -c 'CREATE USER daemon'
+        psql tempus_test_db -c 'GRANT USAGE ON SCHEMA public,tempus TO daemon'
+        psql tempus_test_db -c 'GRANT SELECT ON ALL TABLES IN SCHEMA public,tempus TO daemon'
+    " || die "cannot configure test database"
+echo "test database configured"
+
+echo "installing tempus wps service"
     # service is ran as user daemon, we need     
     # to define TEMPUS_DATA_DIRECTORY for everyone
     if [ -z $(grep TEMPUS_DATA_DIRECTORY /etc/environment) ]; 
     then 
-        echo "echo TEMPUS_DATA_DIRECTORY=/usr/local/share/tempus/data >> /etc/environment " | sudo sh ; 
+        echo "echo TEMPUS_DATA_DIRECTORY=$TEMPUS_DATA_DIRECTORY >> /etc/environment " | sudo sh ; 
     else 
-        echo "found it"; 
+        echo "found $(grep TEMPUS_DATA_DIRECTORY /etc/environment)"; 
     fi
 
-    # install service
+    # install wps service and start it
     sudo chmod a+x /etc/init.d/wps
-    sudo ln -s /etc/init.d/wps /etc/rc2.d/S20wps
-    sudo /etc/init.d/wps start
+    sudo update-rc.d wps defaults 80 
+    sudo /etc/init.d/wps start || die "cannot start wps service"
+echo "tempus wps service has been successfully installed"
 
-    # install qgis plugin for current user
-    mkdir -p ~/.qgis2/python/plugins
-    cp -r $(find ../src -name metadata.txt -printf "%h ") ~/.qgis2/python/plugins
-# end compile and install tempus
-
-# begin nginx configuration
-    # patch nginx config to create 127.0.0.1/wps
+echo "configuring nginx web server for wps service on 127.0.0.1/wps"
+    # patch nginx config
     diff /etc/nginx/sites-available/default \
-        ../data/nginx/sites-available/default \
+        $TEMPUS_DATA_DIRECTORY/nginx/sites-available/default \
         | sudo patch /etc/nginx/sites-available/default
-    sudo /etc/init.d/nginx restart
-# end nginx configuration
+    sudo /etc/init.d/nginx restart || die "cannot start ngnix web server"
+echo "nginx web server has been configured"
+
+echo "installing qgis plugins for current user"
+    mkdir -p ~/.qgis2/python/plugins 
+    cp -r $(find tempus-0.9.0/src -name metadata.txt -printf "%h ") ~/.qgis2/python/plugins || die "cannot install plugins for current user" 
+echo "qgis plugins have been installed for current user"
+
+exit 0
