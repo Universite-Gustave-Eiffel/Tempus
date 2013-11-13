5390623e38ab7d92e8808cbbbd4a6dca0128e9b7
Hugo Mercier <hugo.mercier@oslandia.com>
2013-11-13 16:24:22 +0100

    Add sample SQL query for elevation

---
+++ src/loader/tempus/sql/dem_elevation.sql
@@ -0,0 +1,44 @@
+-- example of query used to elevate road node points
+update tempus.road_node
+set geom=geomz
+from
+(
+select
+	id, st_setsrid(st_makepoint(st_x(pt.geom), st_y(pt.geom), st_value( rast, pt.geom )), 2154) as geomz
+from
+	dem
+join
+	tempus.road_node as pt
+on
+	st_intersects( rast, geom )
+) t
+where t.id = road_node.id
+
+-- example of query used to elevate road sections
+update tempus.road_section
+set geom=geomz
+from
+(
+with pts
+as
+(
+select
+	id, (st_dumppoints(rs.geom)).geom as geom
+	from
+	tempus.road_section as rs
+)
+select
+	id,
+	st_makeline( 
+	st_setsrid(st_makepoint(st_x(geom), st_y(geom), st_value(rast, geom)), 2154)
+	) as geomz
+from
+	pts
+join
+	dem
+on
+	st_intersects(rast, geom)
+group by
+	id
+) t
+where t.id = road_section.id
