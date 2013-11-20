48a5a01ae84607e5dd3e319bc8608ce8604fffff
Hugo Mercier <hugo.mercier@oslandia.com>
2013-11-20 14:45:51 +0000

    Fix QGIS plugin description

--- src/intralys/__init__.py
+++ src/intralys/__init__.py
@@ -34,7 +34,7 @@ def name():
 def description():
     return "Allows automatic refresh of the canvas"
 def version():
-    return "Version 0.1"
+    return "Version 1.0"
 def icon():
     return "icon.png"
 def qgisMinimumVersion():
