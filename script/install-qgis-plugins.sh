c82feecaa3a636f875ed7be8906072026719e94d
Hugo Mercier <hugo.mercier@oslandia.com>
2013-11-21 17:07:32 +0100

    Fix QGIS plugin installation on Linux

--- script/install-qgis-plugins.sh
+++ script/install-qgis-plugins.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 base=$(dirname $0)
 mkdir -p ~/.qgis2/python/plugins/ifsttarrouting
-cp -r $base/../src/hmi_qgis/* ~/.qgis2/python/plugins/ifsttarrouting
+cp -r $base/IfsttarRouting/* ~/.qgis2/python/plugins/ifsttarrouting
 mkdir -p ~/.qgis2/python/plugins/intralys
-cp -r $base/../src/intralys/* ~/.qgis2/python/plugins/intralys
+cp -r $base/intralys/* ~/.qgis2/python/plugins/intralys
