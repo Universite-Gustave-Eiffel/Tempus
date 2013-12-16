7712ac7bdfaf492c0073501959f57e4c04a119ef
Hugo Mercier <hugo.mercier@oslandia.com>
2013-12-16 16:02:59 +0100

    refs #267 use ST_Force3DZ (2.0) instead of ST_Force_3DZ (2.1)

--- src/loader/tempus/sql/navteq.sql
+++ src/loader/tempus/sql/navteq.sql
@@ -36,7 +36,7 @@ INSERT INTO tempus.road_node
 
 update tempus.road_node
 set
-        geom = ST_Force3DZ(ST_SetSRID(ST_StartPoint(st.geom),2154))
+        geom = ST_Force_3DZ(ST_SetSRID(ST_StartPoint(st.geom),2154))
 from
         _tempus_import.streets as st
 where
@@ -44,7 +44,7 @@ where
 
 update tempus.road_node
 set
-        geom = ST_Force3DZ(ST_Transform(ST_EndPoint(st.geom),2154))
+        geom = ST_Force_3DZ(ST_Transform(ST_EndPoint(st.geom),2154))
 from
         _tempus_import.streets as st
 where
