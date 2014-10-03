a5a2f50f8a0703b9942d51f6fb0d2a19aaefd39b
Hugo Mercier <hugo.mercier@oslandia.com>
2014-10-03 16:20:41 +0100

    Delete prefs file after installing the QGIS plugin

--- script/install-qgis-plugins.sh
+++ script/install-qgis-plugins.sh
@@ -4,3 +4,5 @@ mkdir -p ~/.qgis2/python/plugins/ifsttarrouting
 cp -r $base/IfsttarRouting/* ~/.qgis2/python/plugins/ifsttarrouting
 mkdir -p ~/.qgis2/python/plugins/intralys
 cp -r $base/intralys/* ~/.qgis2/python/plugins/intralys
+rm -f ~/.ifsttarrouting.prefs
+
--- win32/install_qgis_plugins.bat
+++ win32/install_qgis_plugins.bat
@@ -2,3 +2,4 @@
 SET wd=%~dp0
 mkdir "%USERPROFILE%\.qgis2\python\plugins"
 xcopy /Y /S /I "%wd%\python\plugins" "%USERPROFILE%\.qgis2\python\plugins"
+if exist "%USERPROFILE%\.ifsttarrouting.prefs" del "%USERPROFILE%\.ifsttarrouting.prefs"
