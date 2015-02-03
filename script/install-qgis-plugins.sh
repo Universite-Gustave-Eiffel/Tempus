0cb61f72424c66b0ba4289e8a1f9faafba6c5cd0
Hugo Mercier <hugo.mercier@oslandia.com>
2015-02-03 10:22:29 +0100

    Fixplugin install script

--- script/install-qgis-plugins.sh
+++ script/install-qgis-plugins.sh
@@ -2,7 +2,5 @@
 base=$(dirname $0)
 mkdir -p ~/.qgis2/python/plugins/ifsttarrouting
 cp -r $base/IfsttarRouting/* ~/.qgis2/python/plugins/ifsttarrouting
-mkdir -p ~/.qgis2/python/plugins/intralys
-cp -r $base/intralys/* ~/.qgis2/python/plugins/intralys
 rm -f ~/.ifsttarrouting.prefs
 
