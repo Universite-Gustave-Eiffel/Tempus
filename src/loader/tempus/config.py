46bc440f3bd33209f9e5fe244c4f6f3a1a514ed6
Hugo Mercier <hugo.mercier@oslandia.com>
2015-09-01 13:38:22 +0100

    Simplify config script and make it work uner windows

--- src/loader/tempus/config.py
+++ src/loader/tempus/config.py
@@ -28,42 +28,8 @@ IMPORTSCHEMA = "_tempus_import"
 # Path where binaries are located (used below)
 BINPATH=""
 
-# PostgreSQL's console client binary location
-PSQL=BINPATH + ""
-
-# PostGIS shp2pgsql tool
-SHP2PGSQL=BINPATH + ""
-
-# DO NOT MODIFY UNDER THIS LINE
-# Autoconfiguration attempts
-import os
 import sys
 
-def is_exe(fpath):
-    print "is_exe", fpath
-    if sys.platform.startswith('linux'):
-        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
-    else:
-        return os.path.isfile(fpath)
+PSQL="psql" + ".exe" if sys.platform.startswith('win') else ""
+SHP2PGSQL="shp2pgsql" + ".exe" if sys.platform.startswith('win') else ""
 
-def which(program):
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
-
-    return None
-
-if not is_exe(PSQL):
-    PSQL = which("psql" + ".exe" if sys.platform.startswith('win') else '')
-
-if not is_exe(SHP2PGSQL):
-    SHP2PGSQL = which("shp2pgsql" + ".exe" if sys.platform.startswith('win') else '')
-
-if PSQL is None or SHP2PGSQL is None:
-    raise OSError("Could not find psql and shp2pgsql.")
