6bb11378ca6dfb46d703336403cc8f1e2f889fb0
Hugo Mercier <hugo.mercier@oslandia.com>
2016-01-15 10:13:43 +0100

    Make graph_dumper compilable under windows

--- src/tools/graph_dumper.cc
+++ src/tools/graph_dumper.cc
@@ -9,10 +9,9 @@
 #include "routing_data.hh"
 #include "routing_data_builder.hh"
 
+#ifdef __linux__
 #include <sys/resource.h>
 #include <malloc.h>
-
-using namespace Tempus;
 
 // current heap usage (allocated minus freed), including overhead
 static size_t heap_usage;
@@ -69,17 +68,14 @@ MemInfo get_memory_usage()
     info.rss = rss * 4 * 1024;
     info.data = data * 4 * 1024;
     return info;
-#if 0
-    rusage r;
-    getrusage( RUSAGE_SELF, &r );
-    float real_size = r.ru_maxrss / 1024.;
-#endif
 }
+#endif
 
 int main( int argc, char* argv[] )
 { 
     namespace po = boost::program_options;
     using namespace std;
+    using namespace Tempus;
 
     std::string db_options = "dbname=tempus_test_db";
     std::string schema = "tempus";
@@ -126,22 +122,24 @@ int main( int argc, char* argv[] )
         dump_file = vm["dump_file"].as<string>();
     }
 
+#ifdef __linux__
     MemInfo info;
-
     info = get_memory_usage();
     size_t real_size = info.rss /1024 /1024;
     size_t data_size = info.data /1024 /1024;
     std::cerr << "RSS: " << real_size << "MB" << std::endl;
     std::cerr << "Data: " << data_size << "MB" << std::endl;
+    heap_usage = 0;
+#endif
 
     {
-        heap_usage = 0;
         TextProgression progression;
         VariantMap options;
         options["db/options"] = Variant::from_string(db_options);
         options["db/schema"] = Variant::from_string(schema);
         const RoutingData* rd = load_routing_data( graph_type, progression, options );
 
+#ifdef __linux__
         info = get_memory_usage();
         real_size = info.rss /1024 /1024;
         data_size = info.data /1024 /1024;
@@ -154,7 +152,7 @@ int main( int argc, char* argv[] )
 
         float frag = (1.0 - float(heap_usage) / info.rss) * 100.0;
         std::cerr << "Fragmentation: " << frag << "%" << std::endl;
-
+#endif
         
         std::cout << "dumping to " << dump_file << " ... " << std::endl;
         dump_routing_data( rd, dump_file, progression );
