c57b42613c387346dbdd814731f15ce077e2d71b
Hugo Mercier <hugo.mercier@oslandia.com>
2016-09-12 16:38:34 +0200

    Split multi edges

--- src/loader/osm2tempus/main.cpp
+++ src/loader/osm2tempus/main.cpp
@@ -1,6 +1,9 @@
 #include <unordered_map>
+#include <unordered_set>
 #include <iostream>
 #include <iomanip>
+#include <functional> // hash
+
 
 #include "db.hh"
 
@@ -9,6 +12,22 @@
 #pragma GCC diagnostic ignored "-Wcast-qual"
 #include "osmpbfreader.h"
 #pragma GCC diagnostic pop
+
+// a pair of nodes
+using node_pair = std::pair<uint64_t, uint64_t>;
+
+namespace std {
+  template <> struct hash<node_pair>
+  {
+    size_t operator()(const node_pair & np) const
+    {
+        // cf http://stackoverflow.com/questions/2590677/how-do-i-combine-hash-values-in-c0x
+        size_t h1 = hash<uint64_t>()( np.first );
+        size_t h2 = hash<uint64_t>()( np.second );
+        return h2 + 0x9e3779b9 + (h1<<6) + (h1>>2);
+    }
+  };
+}
 
 namespace osm_pbf = CanalTP;
 
@@ -165,7 +184,7 @@ public:
         db.exec( "copy edges(node_from, node_to, geom) from stdin with delimiter ';'" );
     }
     
-    virtual void write_section( uint64_t node_from, uint64_t node_to, const std::vector<Point>& points, const osm_pbf::Tags& tags )
+    virtual void write_section( uint64_t node_from, uint64_t node_to, const std::vector<Point>& points, const osm_pbf::Tags& /*tags*/ )
     {
         std::ostringstream data;
         data << node_from << ";" << node_to << ";" << binary_to_hextext( linestring_to_ewkb( points ) ) << std::endl;
@@ -250,8 +269,8 @@ struct PbfReader
 
         auto r = ways.emplace( osmid, Way() );
         Way& w = r.first->second;
-        w.nodes = nodes;
         w.tags = tags;
+        w.nodes = nodes;
     }
 
     void mark_points_and_ways()
@@ -279,7 +298,6 @@ struct PbfReader
             const Way& way = way_it->second;
             if ( way.ignored )
                 continue;
-            //uint64_t osmid = way_it.first;
 
             way_to_sections( way, writer );
         }
@@ -291,23 +309,23 @@ struct PbfReader
         bool section_start = true;
         uint64_t old_node = way.nodes[0];
         uint64_t node_from;
-        std::vector<Point> section_pts;
-        Point old_pt = points.find( old_node )->second;
+        std::vector<uint64_t> section_nodes;
+        //Point old_pt = points.find( old_node )->second;
         for ( size_t i = 1; i < way.nodes.size(); i++ ) {
             uint64_t node = way.nodes[i];
             const Point& pt = points.find( node )->second;
             if ( section_start ) {
-                section_pts.clear();
-                section_pts.push_back( old_pt );
+                section_nodes.clear();
+                section_nodes.push_back( old_node );
                 node_from = old_node;
                 section_start = false;
             }
-            section_pts.push_back( pt );
+            section_nodes.push_back( node );
             if ( i == way.nodes.size() - 1 || pt.uses > 1 ) {
-                writer.write_section( node_from, node, section_pts, way.tags );
+                split_into_sections( node_from, node, section_nodes, way.tags, writer );
+                //writer.write_section( node_from, node, section_pts, way.tags );
                 section_start = true;
             }
-            old_pt = pt;
             old_node = node;
         }
     }
@@ -316,9 +334,69 @@ struct PbfReader
     {
     }
 
+private:
     std::unordered_map<uint64_t, Point> points;
     std::unordered_map<uint64_t, Way> ways;
+
+    // structure used to detect multi edges
+    std::unordered_set<node_pair> way_node_pairs;
+    //std::unordered_map<node_pair, uint64_t> way_node_pairs;
     uint64_t way_id = 0;
+
+    // node ids that are introduced to split multi edges
+    // we count them backward from 2^64 - 1
+    // this should not overlap current OSM node ID (~ 2^32 in july 2016)
+    uint64_t last_artificial_node_id = 0xFFFFFFFFFFFFFFFFLL;
+
+        ///
+    /// Check if a section with the same (from, to) pair exists
+    /// In which case, it is split into two sections
+    /// in order to avoid multigraphs
+    void split_into_sections( uint64_t node_from, uint64_t node_to, const std::vector<uint64_t>& nodes, const osm_pbf::Tags& tags, Writer& writer )
+    {
+        // in order to avoid multigraphs
+        node_pair p ( node_from, node_to );
+        if ( way_node_pairs.find( p ) != way_node_pairs.end() ) {
+            // split the way
+            // if there are more than two nodes, just split on a node
+            std::vector<Point> before_pts, after_pts;
+            uint64_t center_node;
+            if ( nodes.size() > 2 ) {
+                size_t center = nodes.size() / 2;
+                center_node = nodes[center];
+                for ( size_t i = 0; i <= center; i++ ) {
+                    before_pts.push_back( points.find( nodes[i] )->second );
+                }
+                for ( size_t i = center; i < nodes.size(); i++ ) {
+                    after_pts.push_back( points.find( nodes[i] )->second );
+                }
+            }
+            else {
+                const Point& p1 = points.find( nodes[0] )->second;
+                const Point& p2 = points.find( nodes[1] )->second;
+                Point center_point( ( p1.lon + p2.lon ) / 2.0, ( p1.lat + p2.lat ) / 2.0 );
+                
+                before_pts.push_back( points.find( nodes[0] )->second );
+                before_pts.push_back( center_point );
+                after_pts.push_back( center_point );
+                after_pts.push_back( points.find( nodes[1] )->second );
+
+                // add a new point
+                center_node = last_artificial_node_id;
+                points[last_artificial_node_id--] = center_point;
+            }
+            writer.write_section( node_from, center_node, before_pts, tags );
+            writer.write_section( center_node, node_to, after_pts, tags );
+        }
+        else {
+            way_node_pairs.insert( p );
+            std::vector<Point> section_pts;
+            for ( uint64_t node: nodes ) {
+                section_pts.push_back( points.find( node )->second );
+            }
+            writer.write_section( node_from, node_to, section_pts, tags );
+        }
+    }
 };
 
 int main(int argc, char** argv)
