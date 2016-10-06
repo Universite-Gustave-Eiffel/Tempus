64ef850e815105f99cdfdf6c5e9d9634630efea4
Hugo Mercier <hugo.mercier@oslandia.com>
2016-10-06 11:59:14 +0200

    Add a simple isochrone plugin

---
+++ src/plugins/isochrone/isochrone_plugin.cpp
@@ -0,0 +1,171 @@
+#include "plugin_factory.hh"
+#include "isochrone_plugin.h"
+#include "mm_lib/cost_calculator.hh"
+#include "mm_lib/algorithms.hh"
+
+#include <functional>
+
+namespace Tempus
+{
+
+IsochronePlugin::IsochronePlugin( ProgressionCallback& progression, const VariantMap& options )
+    : Plugin( "isochrone_plugin", options )
+{
+    // load graph
+    const RoutingData* rd = load_routing_data( "multimodal_graph", progression, options );
+    graph_ = dynamic_cast<const Multimodal::Graph*>( rd );
+    if ( graph_ == nullptr ) {
+        throw std::runtime_error( "Problem loading the multimodal graph" );
+    }
+}
+
+std::unique_ptr<PluginRequest> IsochronePlugin::request( const VariantMap& options ) const
+{
+    return std::unique_ptr<PluginRequest>( new IsochronePluginRequest( this, options, graph_ ) );
+}
+
+IsochronePluginRequest::IsochronePluginRequest( const IsochronePlugin* plugin, const VariantMap& options, const Multimodal::Graph* graph )
+    : PluginRequest( plugin, options ), graph_(graph)
+{
+}
+
+// Data structure used to label vertices : (vertex, transport_mode)
+struct VertexLabel
+{
+    Multimodal::Vertex vertex;
+    db_id_t mode;
+
+    bool operator<( const VertexLabel& other ) const
+    {
+        return vertex == other.vertex ? mode < other.mode : vertex < other.vertex;
+    }
+    bool operator==( const VertexLabel& other ) const
+    {
+        return vertex == other.vertex && mode == other.mode;
+    }
+};
+} // namespace Tempus
+
+namespace std
+{
+template <>
+struct hash<Tempus::VertexLabel>
+{
+    size_t operator()( const Tempus::VertexLabel& d ) const
+    {
+        return hash<Tempus::Multimodal::Vertex>()( d.vertex ) ^ ( hash<Tempus::db_id_t>()( d.mode ) << 1 );
+    }
+};
+}
+
+namespace Tempus
+{
+//
+// Data structure used inside the dijkstra-like algorithm
+class MMVertexData
+{
+    DECLARE_RW_PROPERTY( potential, float );
+    DECLARE_RW_PROPERTY( wait_time, float );
+    DECLARE_RW_PROPERTY( shift_time, float );
+    DECLARE_RW_PROPERTY( trip, db_id_t );
+    DECLARE_RW_PROPERTY( predecessor, VertexLabel );
+public:
+    MMVertexData() : // default values
+        potential_( std::numeric_limits<float>::max() ),
+        wait_time_( 0.0 ),
+        shift_time_( 0.0 ),
+        trip_( 0 )
+    {}
+};
+
+typedef std::unordered_map<VertexLabel, MMVertexData> MMVertexDataMap;
+
+// IsochroneVisitor
+struct IsochroneVisitor
+{
+public:
+    // exception thrown when the isochrone limit is reached
+    struct StopException {};
+    
+    IsochroneVisitor( const MMVertexDataMap& v_map, double limit, size_t& iterations ) : v_map_( v_map ), iterations_( iterations ), limit_( limit ) {}
+
+    void finish_vertex( const VertexLabel& l, const Multimodal::Graph& )
+    {
+        auto it = v_map_.find( l );
+        if (( it != v_map_.end() ) && ( it->second.potential() >= limit_ ))
+            throw StopException();
+        iterations_++;
+    }
+
+    void examine_vertex( const VertexLabel&, const Multimodal::Graph& ) {}
+    void discover_vertex( const VertexLabel&, const Multimodal::Graph& ) {}
+    void examine_edge( const Multimodal::Edge&, const Multimodal::Graph& ) {}
+    void edge_relaxed( const Multimodal::Edge&, unsigned int /*mode*/, const Multimodal::Graph& ) {}
+    void edge_not_relaxed( const Multimodal::Edge&, unsigned int /*mode*/, const Multimodal::Graph& ) {}
+private:
+    MMVertexDataMap v_map_;
+    size_t iterations_;
+    double limit_;
+};
+
+std::unique_ptr<Result> IsochronePluginRequest::process( const Request& request )
+{
+    std::unique_ptr<Result> result( new Result );
+
+    double isochrone_limit = get_float_option( "Isochrone/limit" );
+    double min_transfer_time = get_float_option( "Time/min_transfer_time" );
+    double walking_speed = get_float_option( "Time/walking_speed" );
+    double cycling_speed = get_float_option( "Time/cycling_speed" );
+    double car_parking_search_time = get_float_option( "Time/car_parking_search_time" );
+
+    Multimodal::Vertex origin = Multimodal::Vertex( *graph_, graph_->road_vertex_from_id(request.origin()).get(), Multimodal::Vertex::road_t() );
+
+    // start time of the query (in minutes)
+    //double start_time = request.steps()[1].constraint().date_time().time_of_day().total_seconds()/60;
+
+    TimetableMap timetable_map, rtimetable_map;
+    FrequencyMap frequency_map, rfrequency_map;
+    CostCalculator cost_calculator( timetable_map,
+                                    rtimetable_map,
+                                    frequency_map,
+                                    rfrequency_map,
+                                    request.allowed_modes(),
+                                    walking_speed,
+                                    cycling_speed,
+                                    min_transfer_time,
+                                    car_parking_search_time,
+                                    boost::none );
+
+    
+    MMVertexDataMap vertex_data_map;
+    boost::associative_property_map< MMVertexDataMap > vertex_data_pmap( vertex_data_map );
+
+    VertexLabel origin_l = {origin, request.allowed_modes()[0]};
+    MMVertexData d;
+    d.set_potential( 0.0 );
+    d.set_predecessor( origin_l );
+    vertex_data_map[origin_l] = d;
+    
+    size_t iterations = 0;
+    IsochroneVisitor vis( vertex_data_map, isochrone_limit, iterations );
+    combined_ls_algorithm_no_init( *graph_, origin_l, vertex_data_pmap, cost_calculator, request.allowed_modes(), vis );
+
+    result->push_back( Isochrone() );
+    Isochrone& isochrone = result->back().isochrone();
+    for ( const auto& v : vertex_data_map ) {
+        Point3D pt = v.first.vertex.coordinates();
+        isochrone.emplace_back( pt.x(), pt.y(), v.first.mode, v.second.potential() );
+    }
+    
+    return result;
+}
+
+
+} // namespace Tempus
+
+DECLARE_TEMPUS_PLUGIN( "isochrone_plugin", Tempus::IsochronePlugin )
+
+
+
+
+
---
+++ src/plugins/isochrone/isochrone_plugin.h
@@ -0,0 +1,91 @@
+/**
+ *   Copyright (C) 2016 Oslandia <infos@oslandia.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef TEMPUS_PLUGIN_ISOCHRONE_H
+#define TEMPUS_PLUGIN_ISOCHRONE_H
+
+#include "plugin.hh"
+
+namespace Tempus {
+
+///
+/// Plugin that computes a (multimodal) isochrone
+class IsochronePlugin : public Plugin
+{
+public:
+    ///
+    /// Constructor of the plugin, called by the core with plugin options
+    IsochronePlugin( ProgressionCallback& progression, const VariantMap& options );
+
+    ///
+    /// Options of the plugin
+    static Plugin::OptionDescriptionList option_descriptions()
+    {
+        Plugin::OptionDescriptionList odl = Plugin::common_option_descriptions();
+        odl.declare_option( "Isochrone/limit", "Limit value of the optimized cost", Variant::from_float(10.0));
+        odl.declare_option( "Time/min_transfer_time", "Minimum time necessary for a transfer to be done (in min)", Variant::from_int(2));
+        odl.declare_option( "Time/walking_speed", "Average walking speed (km/h)", Variant::from_float(3.6));
+        odl.declare_option( "Time/cycling_speed", "Average cycling speed (km/h)", Variant::from_int(12));
+        odl.declare_option( "Time/car_parking_search_time", "Car parking search time (min)", Variant::from_int(5));
+        return odl;
+    }
+
+    ///
+    /// Capabilities of the plugin
+    static Plugin::Capabilities plugin_capabilities()
+    {
+        Plugin::Capabilities caps;
+        caps.optimization_criteria().push_back( CostId::CostDuration );
+        caps.set_depart_after( true );
+        // Not yet supported
+        // caps.set_arrive_before( true );
+        return caps;
+    }
+
+    ///
+    /// Access to the underlying routing data (graph + auxliary data)
+    const RoutingData* routing_data() const { return graph_; }
+
+    ///
+    /// Method the creates a PluginRequest object to handle the request
+    virtual std::unique_ptr<PluginRequest> request( const VariantMap& options = VariantMap() ) const override;
+
+private:
+    const Multimodal::Graph* graph_;
+};
+
+///
+/// Class that handles the isochrone request
+class IsochronePluginRequest : public PluginRequest
+{
+public:
+    ///
+    /// Constructor
+    IsochronePluginRequest( const IsochronePlugin* plugin, const VariantMap& options, const Multimodal::Graph* );
+
+    ///
+    /// The main processing method
+    virtual std::unique_ptr<Result> process( const Request& request ) override;
+
+private:
+    const Multimodal::Graph* graph_;
+};
+
+}
+
+#endif
