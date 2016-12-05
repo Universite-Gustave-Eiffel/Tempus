214d6327e8e5cea8c1283f556290a6b1f7f32f1f
Hugo Mercier <hugo.mercier@oslandia.com>
2016-12-05 06:35:39 -0800

    Fix windows build

--- ci/windows/build_gitlab.bat
+++ ci/windows/build_gitlab.bat
@@ -1,5 +1,6 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
-set PATH=%PATH%;C:\Program Files\CMake\bin
+set PATH=%PATH%;C:\Program Files\CMake\bin;C:\Program Files\PostgreSQL\9.6\bin
+mkdir build
 cd build
-cmake -G "NMake Makefiles" -D BOOST_LIBRARYDIR=C:\osgeo4w64\lib -D BOOST_INCLUDEDIR=C:\osgeo4w64\include\boost-1_56 -D Boost_COMPILER=-vc110 -D LIBXML2_INCLUDE_DIR=C:\osgeo4w64\include -D LIBXML2_LIBRARIES=C:\osgeo4w64\lib -D BUILD_WPS=OFF -D BUILD_QGIS_PLUGIN=OFF -D BUILD_OSM2TEMPUS=OFF -D BUILD_DOC=OFF ..
+cmake -G "NMake Makefiles" -D BOOST_LIBRARYDIR=C:\local\boost_1_62_0\lib64-msvc-14.0 -D BOOST_INCLUDEDIR=C:\local\boost_1_62_0 -D Boost_COMPILER=-vc140 -D LIBXML2_INCLUDE_DIR=C:\osgeo4w64\include -D LIBXML2_LIBRARIES=C:\osgeo4w64\lib -D BUILD_WPS=OFF -D BUILD_QGIS_PLUGIN=OFF -D BUILD_OSM2TEMPUS=OFF -D BUILD_DOC=OFF ..
 nmake
