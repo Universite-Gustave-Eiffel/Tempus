a807dd829f71d1e779ac334c2324222380446b12
Hugo Mercier <hugo.mercier@oslandia.com>
2016-12-14 16:35:36 +0100

    Add protobuf compilation

--- ci/windows/build_gitlab.bat
+++ ci/windows/build_gitlab.bat
@@ -2,5 +2,5 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 set PATH=%PATH%;C:\Program Files\CMake\bin;C:\Program Files\PostgreSQL\9.6\bin
 mkdir build
 cd build
-cmake -G "NMake Makefiles" -D BOOST_LIBRARYDIR=C:\local\boost_1_62_0\lib64-msvc-14.0 -D BOOST_INCLUDEDIR=C:\local\boost_1_62_0 -D Boost_COMPILER=-vc140 -D LIBXML2_INCLUDE_DIR=C:\osgeo4w64\include -D LIBXML2_LIBRARIES=C:\osgeo4w64\lib -D BUILD_WPS=OFF -D BUILD_QGIS_PLUGIN=OFF -D BUILD_OSM2TEMPUS=OFF -D BUILD_DOC=OFF ..
+cmake -G "NMake Makefiles" -D BOOST_LIBRARYDIR=C:\local\boost_1_62_0\lib64-msvc-14.0 -D BOOST_INCLUDEDIR=C:\local\boost_1_62_0 -D Boost_COMPILER=-vc140 -D LIBXML2_INCLUDE_DIR=C:\osgeo4w64\include -D LIBXML2_LIBRARIES=C:\osgeo4w64\lib -D BUILD_WPS=OFF -D BUILD_QGIS_PLUGIN=OFF -D BUILD_OSM2TEMPUS=ON -D BUILD_DOC=OFF -DPROTOBUF_INCLUDE_DIR=C:\libs\include ..
 nmake
