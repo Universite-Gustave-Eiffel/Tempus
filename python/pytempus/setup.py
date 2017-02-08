001f78a1842e63ca15f8c48c4b9d696055bc7083
Hugo Mercier <hugo.mercier@oslandia.com>
2017-02-08 09:30:43 +0100

    Python directory reorganization
    Fix header installation

---
+++ python/pytempus/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, Extension
+
+setup(
+    name='pytempus',
+    version='2.1.0',
+    description="Tempus Python API",
+    long_description="Tempus Python API module",
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: System :: Software Distribution",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    keywords='',
+    author='Tempus Team',
+    author_email='infos@oslandia.com',
+    maintainer='Oslandia',
+    maintainer_email='infos@oslandia.com',
+
+    license='LGPL',
+    ext_modules=[Extension('pytempus',
+                           include_dirs = ['/usr/local/include/tempus', '/usr/include/tempus'],
+                           extra_compile_args=['-std=c++11'],
+                           libraries = ['tempus', 'boost_python'],
+                           library_dirs = ['/usr/local/lib', '/usr/lib'],
+                           sources = ['src/pytempus.cc'])],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=(),
+    extras_require={
+        "develop": ()
+    }
+)
