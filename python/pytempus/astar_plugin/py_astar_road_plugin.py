db5161587d01a0ddc3122467f2820512290ae057
Hugo Mercier <hugo.mercier@oslandia.com>
2017-02-09 14:48:44 +0100

    Add license headers

--- python/pytempus/astar_plugin/py_astar_road_plugin.py
+++ python/pytempus/astar_plugin/py_astar_road_plugin.py
@@ -1,3 +1,42 @@
+"""
+/**
+ *   Copyright (C) 2012-2017 Oslandia <infos@oslandia.com>
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
+ *   
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+"""
+
+"""
+/**
+ *   Copyright (C) 2012-2017 Oslandia <infos@oslandia.com>
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
+ *   
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+"""
+
+# This is an example of a Tempus plugin written in Python
+# See the standalone Python application to test it
+
 import pytempus as tempus
 
 from graph_tool.all import *
--- python/pytempus/src/pytempus.cc
+++ python/pytempus/src/pytempus.cc
@@ -1,3 +1,19 @@
+/**
+ *   Copyright (C) 2012-2017 Oslandia <infos@oslandia.com>
+ *
+ *   This library is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU Library General Public
+ *   License as published by the Free Software Foundation; either
+ *   version 2 of the License, or (at your option) any later version.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *   Library General Public License for more details.
+ *   You should have received a copy of the GNU Library General Public
+ *   License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <list>
 #include <vector>
 
@@ -6,7 +22,6 @@
 
 
 #include <tempus/plugin.hh>
-// #include "reverse_multimodal_graph.hh"
 namespace bp = boost::python;
 
 /* Helper to register std::vector<T> <-> python list[T] */
