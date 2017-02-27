f2446a4cde78b6abbfbd42fca2aa5cd28c6e26ee
Hugo Mercier <hugo.mercier@oslandia.com>
2017-02-27 15:51:52 +0100

    Fix pg_config path

--- ci/windows/build_gitlab.bat
+++ ci/windows/build_gitlab.bat
@@ -2,5 +2,22 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 set PATH=%PATH%;C:\Program Files\CMake\bin;C:\Program Files\PostgreSQL\9.6\bin
 mkdir build
 cd build
-cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo -D BOOST_LIBRARYDIR=C:\local\boost_1_62_0\lib64-msvc-14.0 -D BOOST_INCLUDEDIR=C:\local\boost_1_62_0 -D Boost_COMPILER=-vc140 -D LIBXML2_INCLUDE_DIR=C:\osgeo4w64\include -D LIBXML2_LIBRARIES=C:\osgeo4w64\lib -D BUILD_WPS=OFF -D BUILD_QGIS_PLUGIN=OFF -D BUILD_OSM2TEMPUS=ON -D BUILD_DOC=OFF -DPROTOBUF_INCLUDE_DIR=C:\osgeo4w64\include -DPROTOBUF_LIBRARY=C:\osgeo4w64\lib\libprotobuf.lib -DPROTOBUF_LITE_LIBRARY=C:\osgeo4w64\lib\libprotobuf-lite.lib -DPROTOBUF_PROTOC_EXECUTABLE=c:\osgeo4w64\bin\protoc.exe .. -DCMAKE_INSTALL_PREFIX=c:\cygwin64\home\ci
+cmake -G "NMake Makefiles" ^
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo ^
+  -DCMAKE_INSTALL_PREFIX=c:\cygwin64\home\ci ^
+  "-DPOSTGRESQL_PG_CONFIG=C:\Program Files\Postgresql\9.6\bin\pg_config.exe" ^
+  -DBOOST_LIBRARYDIR=C:\local\boost_1_62_0\lib64-msvc-14.0 ^
+  -DBOOST_INCLUDEDIR=C:\local\boost_1_62_0 ^
+  -DBoost_COMPILER=-vc140 ^
+  -DLIBXML2_INCLUDE_DIR=C:\osgeo4w64\include ^
+  -DLIBXML2_LIBRARIES=C:\osgeo4w64\lib ^
+  -DBUILD_WPS=OFF ^
+  -DBUILD_QGIS_PLUGIN=OFF ^
+  -DBUILD_OSM2TEMPUS=ON ^
+  -DBUILD_DOC=OFF ^
+  -DPROTOBUF_INCLUDE_DIR=C:\osgeo4w64\include ^
+  -DPROTOBUF_LIBRARY=C:\osgeo4w64\lib\libprotobuf.lib ^
+  -DPROTOBUF_LITE_LIBRARY=C:\osgeo4w64\lib\libprotobuf-lite.lib ^
+  -DPROTOBUF_PROTOC_EXECUTABLE=c:\osgeo4w64\bin\protoc.exe ^
+  ..
 nmake
